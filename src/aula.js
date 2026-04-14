870f9409921a5ca42417a09a93386db77bbc17bb
Matheus N S <pfvninguemusaesseemail@gmail.com>
2026-04-14 16:32:01 -0300

    Update and rename Matrizes.java to aula.js

---
+++ src/aula.js
@@ -0,0 +1,32 @@
+
+function variaveis(){   
+const nome = 'Matheus'
+
+let idade = 17
+
+let altura = 1.90
+
+console.log(nome)
+console.log(idade)
+console.log(altura)
+}
+function ola(){
+    console.log('Olá, seja bem-vindo!')
+}
+function classificarNumero(numero){
+
+    if (numero === 0) {
+        console.log(numero + ' é um número neutro.')
+    } else if (numero < 0) {
+        console.log(numero + ' é um número negativo.')
+    } else {
+        console.log(numero + ' é um número positivo.')
+    }
+}
+function nomear(){
+
+const nome = prompt('informe seu nome: ')
+console.log('Olá, ' + nome + '! Seja bem-vindo!')
+
+}
+nomear()
