833f4b3d80a89034905d0788fcf0aed958e7a3a7
Matheus Souza <matheus63466736@alunos.sc.senac.br>
2026-07-14 16:06:08 -0300

    mysql

--- arquitetura_projeto/src/DAO/ProdutoDao.java
+++ arquitetura_projeto/src/DAO/ProdutoDao.java
@@ -4,7 +4,8 @@ import interfaces.ICRUD;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
-import java.sql.Statement;
+import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
 import modelos.Produto;
 import utils.ConectaDB;
@@ -13,54 +14,127 @@ public class ProdutoDao implements ICRUD {
 
     @Override
     public Produto salvar(Produto prod) {
+        String sql = "INSERT INTO tb_produtos(descricao, preco) VALUES (?, ?)";
 
-        String sql = "INSERT INTO tb_produtos (descricao, preco) VALUES (?, ?)";
+        Connection con = ConectaDB.conectar();
 
-        try (
-            Connection conn = ConectaDB.conectar();
-            PreparedStatement stmt = conn.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
-        ) {
+        try {
+            PreparedStatement stm = con.prepareStatement(sql, PreparedStatement.RETURN_GENERATED_KEYS);
 
-            stmt.setString(1, prod.getDescricao());
-            stmt.setDouble(2, prod.getPreco());
+            stm.setString(1, prod.getDescricao());
+            stm.setDouble(2, prod.getPreco());
 
-            int linhas = stmt.executeUpdate();
+            stm.executeUpdate();
 
-            if (linhas > 0) {
-                ResultSet rs = stmt.getGeneratedKeys();
+            ResultSet rs = stm.getGeneratedKeys();
 
-                if (rs.next()) {
-                    prod.setId(rs.getInt(1));
-                }
-
-                System.out.println("Produto salvo com sucesso!");
+            if (rs.next()) {
+                prod.setId(rs.getInt(1));
             }
 
-        } catch (Exception e) {
-            System.out.println("Erro ao salvar produto.");
-            e.printStackTrace();
-        }
+            rs.close();
+            stm.close();
+            con.close();
 
-        return prod;
+            return prod;
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return null;
+        }
     }
 
     @Override
     public void deletar(int id) {
-        System.out.println("Deletando produto: " + id);
+        String sql = "DELETE FROM tb_produtos WHERE id = ?";
+
+        Connection con = ConectaDB.conectar();
+
+        try {
+            PreparedStatement stm = con.prepareStatement(sql);
+            stm.setInt(1, id);
+
+            stm.executeUpdate();
+
+            stm.close();
+            con.close();
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 
     @Override
     public void alterar(Produto prod) {
-        System.out.println("Alterando produto: " + prod.getId());
+        String sql = "update tb_produtos set descricao = ?, preco = ? where id = ?";
+
+        Connection con = ConectaDB.conectar();
+
+        try {
+            PreparedStatement stm = con.prepareStatement(sql);
+
+            stm.setString(1, prod.getDescricao());
+            stm.setDouble(2, prod.getPreco());
+            stm.setInt(3, prod.getId());
+
+            stm.executeUpdate();
+
+            stm.close();
+            con.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 
     @Override
     public Produto consultar(int id) {
-        return null;
+        String sql = "SELECT * FROM tb_produtos WHERE id = ?";
+
+        Connection con = ConectaDB.conectar();
+
+        try {
+            PreparedStatement stm = con.prepareStatement(sql);
+            stm.setInt(1, id);
+
+            ResultSet rs = stm.executeQuery();
+
+            Produto prod = null;
+
+            if (rs.next()) {
+                prod = new Produto();
+                prod.setId(rs.getInt("id"));
+                prod.setDescricao(rs.getString("descricao"));
+                prod.setPreco(rs.getDouble("preco"));
+            }
+
+            rs.close();
+            stm.close();
+            con.close();
+
+            return prod;
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return null;
+        }
     }
 
     @Override
     public List<Produto> consultar() {
-        return null;
+        List<Produto> produtos = new ArrayList<Produto>();
+        Connection con = ConectaDB.conectar();
+        String sql = "select * from tb_produtos";
+        try {
+            PreparedStatement stm = con.prepareStatement(sql);
+            ResultSet rs = stm.executeQuery();
+            while (rs.next()) {
+                Produto p = new Produto(rs.getInt(1), rs.getString(2), rs.getDouble(3));
+                produtos.add(p);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return produtos;
     }
+
 }
--- arquitetura_projeto/src/Main.java
+++ arquitetura_projeto/src/Main.java
@@ -3,10 +3,20 @@ import modelos.Produto;
 
 public class Main {
 public static void main(String[] args) {
-    Produto p1 = new Produto("Milho", 20.85);
+    Produto p1 = new Produto("Maca", 5.85);
 
     ProdutoDao dao = new ProdutoDao();
-    Produto retorno = dao.salvar(p1);
-     System.out.println(retorno.getId() + " - " + retorno.getDescricao());
- }
+    //Produto retorno = dao.salvar(p1);
+     //System.out.println(retorno.getId() + " - " + retorno.getDescricao());
+ 
+     //dao.deletar(1);
+    // List<Produto> lista = dao.consultar();
+    // for (Produto p : lista){
+    //    System.out.println(p.getId() + " " + p.getDescricao() + " " + p.getPreco());
+     //}
+     
+     Produto p = dao.consultar(3);
+     System.out.println(p.getId() + " " + p.getDescricao() + " " + p.getPreco());
+
+    }
 }
